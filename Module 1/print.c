bc06988115f3aef1932b6259056c6626ca8f457b
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-02 02:59:09 +0600

    Module 1 initialized

---
+++ Module 1/print.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // printf("Hello!\nI am a programmer.");
+    printf("\\\\\n");
+    printf("100%%%%");
+    return 0;
+}
---
+++ Module 1/variable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    // int rahim;  // declaration
+    // rahim = 100;
+    
+    // int rahim = 100, karim = 200;
+
+    // printf("%d %d", karim, rahim);
+
+    // float num = 23.343;
+    // printf("%0.3f", num);
+
+    char letter = 'A';
+    printf("%c", letter);
+    return 0;
+}
