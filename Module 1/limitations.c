e08b20584b35848bfbc13de8dc614ba3a2537be4
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-02 17:02:15 +0600

    datatype limitations

---
+++ Module 1/limitations.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    // int num1 = 10000000000;
+    // printf("%d", num1);
+
+    long long int num2 = 10000000000;
+    printf("%lld", num2);
+
+    // float a = 23.3434342423;
+    // printf("%f", a);
+
+    // double b = 23.1234567890123456;
+    // printf("%0.14lf", b);
+    return 0;
+}
