e5140f75a7154042e80d737956dfba6c246af3f1
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-02 22:50:45 +0600

    arithmetic operators

---
+++ Module 2/arithmetic.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    // // +
+    // int a = 10, b = 20;
+    // int sum = a + b;
+    // printf("%d", sum);
+
+    // // -
+    // int num1 = 20, num2 = 30;
+    // int substract = num2 - num1;
+    // printf("%d", substract);
+
+    // // *
+    // int num3 = 3, num4 = 5;
+    // int mul = num3 * num4;
+    // printf("%d", mul);
+
+    // // /
+    // float num5 = 20, num6 = 3;
+    // float div = num5 / num6;
+    // printf("%f", div);
+
+    // %
+    int num7 = 3, num8 = 20;
+    int mod = num7 % num8;
+    int mod2 = num8 % num7;
+    printf("%d", mod);
+    printf("%d", mod2);
+    return 0;
+}
