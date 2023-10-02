00896280b61802ecf19b4d73087a3f8704b8035a
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-02 21:15:26 +0600

    scanf more

--- Module 1/output.c
+++ Module 1/output.c
@@ -6,13 +6,26 @@ int main(){
     // scanf("%d %d", &num1, &num2);
     // printf("%d %d", num1, num2);
 
-    scanf("%d%c %d%c", &num1, &p, &num2, &p);
-    printf("%d%c %d%c", num1, p, num2, p);
+    // scanf("%d%c %d%c", &num1, &p, &num2, &p);
+    // printf("%d%c %d%c", num1, p, num2, p);
 
     // scanf("%d%% %d%%", &num1, &num2);
     // printf("%d%% %d%%", num1, num2);
 
     // scanf("%d %d", &num1, &num2);
     // printf("%da %db", num1, num2);
+
+    char name[20];
+    int age;
+    float grade;
+    printf("\nwhat is your name???\n");
+    gets(name);
+    printf("You name: %s\n\n", name);
+    printf("what is your age???\n");
+    scanf("%d", &age);
+    printf("Your age: %d\n\n", age);
+    printf("what is your grade in last exam???\n");
+    scanf("%f", &grade);
+    printf("Your grade: %.2f\n", grade);
     return 0;   
 }
