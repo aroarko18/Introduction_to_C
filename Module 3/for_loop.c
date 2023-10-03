98f2fd79ebf43b398d1d1a3ee980b9689c0ab2f5
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-03 22:49:48 +0600

    loop condition reversed

--- Module 3/for_loop.c
+++ Module 3/for_loop.c
@@ -2,8 +2,13 @@
 
 int main(){
     int i;
-    for(i = 0; i <= 10; i++){
-        printf("Hello\n");
+    // for(i = 1; i <= 10; i++){
+    //     // printf("Hello\n");
+    //     printf("Value of i: %d\n", i);
+    // }
+
+    for(i=10; i>=1; i--){
+        printf("Value of i: %d\n", i);
     }
     return 0;
 }
