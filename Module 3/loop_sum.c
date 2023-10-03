3aeac42d6b794465a42e091486bdf58b34a27386
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-03 23:23:57 +0600

    just used code formatter

--- Module 3/loop_sum.c
+++ Module 3/loop_sum.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     long long int sum = 0;
     int i;
     int n;
     printf("Enter the value untill you would like to get the sum result: ");
     scanf("%d", &n);
-    for(i=1; i<=n; i++){
+    for (i = 1; i <= n; i++)
+    {
         sum += i;
     }
+
     printf("Sum of %d is %lld\n", n, sum);
     return 0;
 }
