65fa0429ffce4e5bc8e2e29c18d0b751e0dee23a
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-05 01:30:01 +0600

    question 1 and 2

---
+++ Module 3.5/sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b;
+    scanf("%d %d", &a, &b);
+    long long int sum = a + b;
+    printf("%lld", sum);
+    return 0;
+}
