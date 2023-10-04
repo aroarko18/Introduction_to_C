d2a1c18e21acc11db09f2ecbddf5675ccadfdf49
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-05 01:11:09 +0600

    quiz practice

---
+++ Module 3/quiz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main()
+{
+    // int k;
+    // for (k = -3; k < -5; k++)
+    //     printf("Hello ");
+
+    // int i = 0;
+    // while (i < 3)
+    // {
+    //     i++;
+    // }
+    // printf("%d", i);
+
+    // int a = 0, i;
+    // for (i = 0; i < 5; i++)
+    // {
+    //     a++;
+    //     continue;
+    // }
+    // printf("%d %d", i, a);
+
+    // int a = 0, i;
+    // for (i = 0; i < 5; i++)
+    // {
+    //     continue;
+    //     a++;
+    // }
+    // printf("%d %d", i, a);
+    // for (int i = 0; i < 20; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    // for (int i = 20 - 1; i >= 0; i -= 1)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    int i = 0;
+    while (i = 0)
+    {
+        printf("Hello\n");
+    }
+    return 0;
+}
