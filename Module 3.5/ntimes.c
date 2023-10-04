18612fd34c270e69464a4d5b5d25023a8869f44b
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-05 01:32:12 +0600

    ntimes using loop

--- Module 3.5/ntimes.c
+++ Module 3.5/ntimes.c
@@ -7,5 +7,11 @@ int main()
 {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int i, n;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("I Love Practice\n");
+    }
     return 0;
 }
