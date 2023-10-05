39e630eea46b687d2d2fa788d252d0020b961e0c
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-06 00:51:52 +0600

    tricky multiplication

---
+++ Module 4/question2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    long long int a, b;
+    scanf("%lld %lld", &a, &b);
+
+    long long int multiply = a * b;
+    printf("%lld", multiply);
+    return 0;
+}
