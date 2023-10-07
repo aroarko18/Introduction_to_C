169e70eb235fc4b40bc669ef34e8d10d385c7694
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-08 02:12:19 +0600

    max min solution

---
+++ Module 5/max-min.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    // min value finder
+    if (a <= b && a <= c)
+    {
+        printf("%d ", a);
+    }
+    else if (b <= a && b <= c)
+    {
+        printf("%d ", b);
+    }
+    else
+    {
+        printf("%d ", c);
+    }
+
+    // max value finder
+    if (a >= b && a >= c)
+    {
+        printf("%d\n", a);
+    }
+    else if (b >= a && b >= c)
+    {
+        printf("%d\n", b);
+    }
+    else
+    {
+        printf("%d\n", c);
+    }
+
+    return 0;
+}
