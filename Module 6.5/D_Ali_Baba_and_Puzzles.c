c0efa5df2f3033ac31719832e02824aa38cc8818
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-10 02:46:25 +0600

    alibaba puzzles

---
+++ Module 6.5/D_Ali_Baba_and_Puzzles.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b, c;
+    scanf("%lld %lld %lld", &a, &b, &c);
+
+    long long int d;
+    scanf("%lld", &d);
+    if (((a + b) - c) == d)
+    {
+        printf("YES\n");
+    }
+    else if (((a - b) + c) == d)
+    {
+        printf("YES\n");
+    }
+    else if ((a + b * c) == d)
+    {
+        printf("YES\n");
+    }
+    else if ((a * b + c) == d)
+    {
+        printf("YES\n");
+    }
+    else if ((a - b * c) == d)
+    {
+        printf("YES\n");
+    }
+    else if ((a * b - c) == d)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
