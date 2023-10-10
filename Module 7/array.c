2485f745708b85cfc8f9d33400044485b6635617
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-11 02:19:56 +0600

    array input output

--- Module 7/array.c
+++ Module 7/array.c
@@ -3,5 +3,17 @@
 int main()
 {
     int arr[10];
+
+    for (int i = 0; i < 5; i++)
+    {
+        int a;
+        scanf("%d", &a);
+        arr[i] = a;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
     return 0;
 }
