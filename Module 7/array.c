08af8e9107c7debdb3984d3c72ee9e9a5996051c
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-11 03:20:01 +0600

    array basic

--- Module 7/array.c
+++ Module 7/array.c
@@ -2,18 +2,21 @@
 
 int main()
 {
-    int arr[3];
+    // int arr[3];
 
-    for (int i = 0; i < 3; i++)
-    {
-        int a;
-        scanf("%d", &a);
-        arr[i] = a;
-    }
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     int a;
+    //     scanf("%d", &a);
+    //     arr[i] = a;
+    // }
 
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%d\n", arr[i]);
-    }
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     printf("%d\n", arr[i]);
+    // }
+
+    int arrayName[] = {1, 2, 3};
+    printf("%d", arrayName[3]);
     return 0;
 }
