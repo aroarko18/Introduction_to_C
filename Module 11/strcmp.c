f349db4172b603c0e6dd9f06d2437bb39d73befb
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-18 19:55:07 +0600

    strcmp shortcut

---
+++ Module 11/strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[100], b[100];
+    scanf("%s %s", a, b);
+
+    int cmp = strcmp(a, b);
+    // printf("%d", cmp);
+    if (cmp < 0)
+    {
+        printf("A Chuto\n");
+    }
+    else if (cmp > 0)
+    {
+        printf("B Chuto\n");
+    }
+    else
+    {
+        printf("Same");
+    }
+    return 0;
+}
