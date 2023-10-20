3e3ec35f61269527e6756459e101bcfdfc5c43ff
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-20 06:40:22 +0600

    farmers day saving

--- Midterm/Farmers.c
+++ Midterm/Farmers.c
@@ -7,5 +7,19 @@ int main()
 {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int t;
+    scanf("%d", &t);
+
+    int m1, m2, d;
+
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d %d", &m1, &m2, &d);
+
+        int new_days = (m1 * d) / (m1 + m2);
+        int saved_days = d - new_days;
+        printf("%d\n", saved_days);
+    }
+
     return 0;
 }
