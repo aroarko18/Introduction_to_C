0c6c6f2b29c48e8cd40188799c4b76c52c3089c6
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-25 19:54:15 +0600

    patter3 in reverse way

--- Module 13/pattern3.c
+++ Module 13/pattern3.c
@@ -3,9 +3,19 @@
 int main()
 {
     int n = 5;
-    scanf("%d", &n);
+    // scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++)
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     // for (int j = 1; j <= i; j++)
+    //     // {
+    //     //     printf("%d ", j);
+    //     // }
+
+    //     printf("\n");
+    // }
+
+    for (int i = n; i > 0; i--)
     {
         for (int j = 1; j <= i; j++)
         {
