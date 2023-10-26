f216ba1872c45ff661e2b5dab6f65bf6cb19739a
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-26 16:42:08 +0600

    math functions

---
+++ Module 14/math_functions.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    // double x;
+    // scanf("%lf", &x);
+    // // int ans = ceil(x);
+    // // int ans = floor(x);
+    // float ans = round(x);
+    // // printf("%d\n", ans);
+    // printf("%0.2f", ans);
+
+    // double x;
+    // scanf("%lf", &x);
+
+    // double ans = sqrt(x);
+    // printf("%0.2lf\n", ans);
+
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+
+    // int ans = pow(a, b);
+    // printf("%d\n", ans);
+
+    int x;
+    scanf("%d", &x);
+    int ans = abs(x);
+    printf("%d", ans);
+    return 0;
+}
