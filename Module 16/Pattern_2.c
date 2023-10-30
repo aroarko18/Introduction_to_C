262cb5f7fe0630ec894ead1371cef23afcef3d5e
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-10-30 21:08:46 +0600

    pattern 2

---
+++ Module 16/Pattern_2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = i; k >= 1; k--)
+        {
+            printf("%d", k);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
